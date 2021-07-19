85ab49937e8c6a562e7b717703097d62d0fe5cec
Julian Vega <jay.log22@gmail.com>
2021-07-19 19:24:46 -0300

    Final Commit
    Final commit on this repository since this tutorial is done.

---
+++ Assets/Scripts/Player Scripts/PlayerMoveTouch.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveTouch : MonoBehaviour
+{
+    public float speed = 8f, maxVelocity = 4f;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private bool moveLeft, moveRight;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        if (moveLeft)
+        {
+            MoveLeft();
+        }
+
+        if (moveRight)
+        {
+            MoveRight();
+        }
+    }
+
+    public void SetMoveLeft(bool moveLeft)
+    {
+        this.moveLeft = moveLeft;
+        this.moveRight = !moveLeft;
+    }
+
+    public void StopMoving()
+    {
+        moveLeft = moveRight = false;
+        anim.SetBool("isWalking", false);
+    }
+
+    void MoveLeft()
+    {
+        float forceX = 0;
+        float velocity = Mathf.Abs(rb.velocity.x);
+
+        if (velocity < maxVelocity)
+        {
+            forceX = -speed;
+        }
+
+        Vector3 temp = transform.localScale;
+        temp.x = -1.3f;
+        transform.localScale = temp;
+
+        anim.SetBool("isWalking", true);
+
+        rb.AddForce(new Vector2(forceX, 0));
+    }
+
+    void MoveRight()
+    {
+        float forceX = 0;
+        float velocity = Mathf.Abs(rb.velocity.x);
+
+        if (velocity < maxVelocity)
+        {
+            forceX = speed;
+        }
+
+        Vector3 temp = transform.localScale;
+        temp.x = 1.3f;
+        transform.localScale = temp;
+
+        anim.SetBool("isWalking", true);
+
+        rb.AddForce(new Vector2(forceX, 0));
+    }
+}
---
+++ Assets/Scripts/Touch Scripts/TouchMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchMovement : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    private PlayerMoveTouch playerMove;
+
+    void Start()
+    {
+        playerMove = GameObject.Find("Player").GetComponent<PlayerMoveTouch>();
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+        playerMove.StopMoving();
+    }
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        if (gameObject.name == "Left")
+        {
+            playerMove.SetMoveLeft(true);
+        }
+        else
+        {
+            playerMove.SetMoveLeft(false);
+        }
+    }
+}
